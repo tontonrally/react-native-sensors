36def41b0666dc795baaba1c181c3e9152c7f75a
Christophe Lemonnier <clemonnier@gmail.com>
2017-04-11 00:49:40 +0100

    adding support of background task on windows

--- windows/Accelerometer.cs
+++ windows/Accelerometer.cs
@@ -2,16 +2,18 @@
 using ReactNative.Bridge;
 using ReactNative.Modules.Core;
 using System;
+using Windows.ApplicationModel.Background;
 using Windows.Foundation;
 using Sensors = Windows.Devices.Sensors;
 
 namespace RNSensors
 {
-    public class Accelerometer : ReactContextNativeModuleBase, ILifecycleEventListener
+    public sealed class Accelerometer : ReactContextNativeModuleBase, ILifecycleEventListener, IBackgroundTask
     {
         private Sensors.Accelerometer _accelerometer;
         private int interval = 100;
         private DateTimeOffset _lastReading = DateTimeOffset.Now;
+        private BackgroundTaskDeferral Deferral;
 
         public Accelerometer(ReactContext reactContext) : base(reactContext)
         {
@@ -27,6 +29,24 @@ namespace RNSensors
         }
 
         private void ReadingChanged(object sender, Sensors.AccelerometerReadingChangedEventArgs e)
+        {
+            Sensors.AccelerometerReading reading = e.Reading;
+
+            if (_lastReading.AddMilliseconds(interval) <= reading.Timestamp)
+            {
+                _lastReading = reading.Timestamp;
+
+                this.SendEvent("Accelerometer", new RNSensorsJsonObject
+                {
+                    X = reading.AccelerationX,
+                    Y = reading.AccelerationY,
+                    Z = reading.AccelerationZ,
+                    Timestamp = reading.Timestamp
+                }.ToJObject());
+            }
+        }
+
+        private void BackgroundReadingChanged(object sender, Sensors.AccelerometerReadingChangedEventArgs e)
         {
             Sensors.AccelerometerReading reading = e.Reading;
 
@@ -72,7 +92,6 @@ namespace RNSensors
             Context.GetJavaScriptModule<RCTDeviceEventEmitter>().emit(eventName, parameters);
         }
 
-
         public void OnDestroy()
         {
             throw new NotImplementedException();
@@ -86,6 +105,25 @@ namespace RNSensors
         public void OnSuspend()
         {
             throw new NotImplementedException();
+        }
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _accelerometer.ReadingChanged += new TypedEventHandler<Sensors.Accelerometer, Sensors.AccelerometerReadingChangedEventArgs>(BackgroundReadingChanged);
+
+            Deferral = taskInstance.GetDeferral();
+
+            taskInstance.Canceled += new BackgroundTaskCanceledEventHandler(OnCanceled);
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if(_accelerometer != null)
+            {
+                _accelerometer.ReadingChanged -= new TypedEventHandler<Sensors.Accelerometer, Sensors.AccelerometerReadingChangedEventArgs>(BackgroundReadingChanged);
+            }
+
+            Deferral.Complete();
         }
     }
 }
